fc8deee20989e316837f5db42a3a143cfa99c978
kanviofficeilika <kanviofficeilika@gmail.com>
2026-04-15 18:21:34 +0530

    april 15 - update 5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
 };
-
 export default nextConfig;
