9e496c3ec13316ecf7fe1a932de97748b166a9e0
kanviofficeilika <kanviofficeilika@gmail.com>
2026-04-30 11:16:45 +0530

    april 30 - update 1 - scrolltotop module

--- components/layout/ScrollToTop.tsx
+++ components/layout/ScrollToTop.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function ScrollToTop() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
